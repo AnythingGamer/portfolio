27f5bd8fc7559b95476935ebcf90b5e728c692fa
AnythingGamer <shubhbuchwani@gmail.com>
2025-02-19 18:37:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+import os
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["my_portfolio"]
@@ -25,5 +26,6 @@ def contactus():
     db.contactus.insert_one(doc)
     return "Thank you for contacting us. We will get back to you soon."
 
-if (__name__ == "__main__"):
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
